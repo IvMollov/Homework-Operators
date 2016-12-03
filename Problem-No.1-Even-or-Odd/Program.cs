8fbe4d1330a02be62b90c9d9cbcb2d86c5cf1cfa
IvMollov <mollov.i@abv.bg>
2016-12-03 13:04:09 +0200

    Add files via upload

---
+++ Problem-No.1-Even-or-Odd/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_No._1_Odd_Or_Even
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         Console.Write("Enter number: ");
+         int number = int.Parse(Console.ReadLine());
+         if (number % 2 == 0)
+         {
+           Console.Write("Number {0} is even.", number);
+         }
+         else
+         {
+           Console.Write("Number {0} is odd.", number);
+         }
+            Console.ReadLine();
+        }
+    }
+}
