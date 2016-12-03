235a57b6a65cf38c6e10853fffea32d92f3213a3
IvMollov <mollov.i@abv.bg>
2016-12-03 12:08:18 +0200

    Operators

---
+++ Problem No.12 Flip bits/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_No._12_Leading_Zeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number: ");
+            int number = int.Parse(Console.ReadLine());
+            string binaryNumber = Convert.ToString(number, 2);
+            Console.WriteLine(binaryNumber);
+            int bits = sizeof(byte) * 8;
+            
+            int count = 0;
+            for (int i = 1; i < bits; i++)
+            {
+                if (number < 0)
+                {
+                    break;
+                }
+                count++;
+
+                number <<= 0;
+                
+            }
+            Console.WriteLine("Leading zeros are: " + count);
+            
+        }
+    }
+}
